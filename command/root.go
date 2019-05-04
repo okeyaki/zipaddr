31dec761ba25ae8c5b7a818a2d6aacceee7b2186
okeyaki <shinya.otsuki.4d@gmail.com>
2019-05-04 19:45:10 +0900

    サブコマンドが指定されなかった場合にヘルプが表示されるようにします

--- command/root.go
+++ command/root.go
@@ -8,9 +8,6 @@ func newRootCommand() *cobra.Command {
 	root := &cobra.Command{
 		Use:   "zipaddr",
 		Short: "TBD",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
-		},
 	}
 
 	root.AddCommand(newNormalizeCommand())
